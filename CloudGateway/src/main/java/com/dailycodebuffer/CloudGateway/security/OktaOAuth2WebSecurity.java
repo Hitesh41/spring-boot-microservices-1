1fb56abac15f7863917ca4e61763c23f216513fe
Shabbir Dawoodi <shabbirdwd53@gmail.com>
2025-03-15 16:03:00 +0530

    Adding Auth0 Security as ResourceServer in Gateway

--- CloudGateway/src/main/java/com/dailycodebuffer/CloudGateway/security/OktaOAuth2WebSecurity.java
+++ CloudGateway/src/main/java/com/dailycodebuffer/CloudGateway/security/OktaOAuth2WebSecurity.java
@@ -3,15 +3,23 @@ package com.dailycodebuffer.CloudGateway.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import java.util.Collection;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -32,6 +40,9 @@ public class OktaOAuth2WebSecurity {
                 .authorizeExchange(authz -> authz
 
                         .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .authorizationRequestResolver(
@@ -56,5 +67,26 @@ public class OktaOAuth2WebSecurity {
     private Consumer<OAuth2AuthorizationRequest.Builder> authorizationRequestCustomizer() {
         return customizer -> customizer
                 .additionalParameters(params -> params.put("audience", audience));
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverterAdapter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        JwtGrantedAuthoritiesConverter defaultConverter = new JwtGrantedAuthoritiesConverter();
+
+        jwtConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Collection authorities = defaultConverter.convert(jwt);
+
+            // Extract roles from a custom claim in Auth0
+            Collection customAuthorities = jwt.getClaimAsStringList("https://dailycodebuffer.com/roles")
+                    .stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+
+            authorities.addAll(customAuthorities);
+            return authorities;
+        });
+
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtConverter);
     }
 }
